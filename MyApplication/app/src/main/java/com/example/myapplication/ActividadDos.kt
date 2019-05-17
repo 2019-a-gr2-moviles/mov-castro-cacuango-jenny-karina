cd5ae81a6539f922fff0501d73473b68329d0e22
Jenny <jennykarinacastro@gmail.com>
2019-05-17 16:01:44 -0500

    Button_log

--- MyApplication/app/src/main/java/com/example/myapplication/ActividadDos.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ActividadDos.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_actividad_dos.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -11,8 +12,19 @@ class ActividadDos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad_dos)
+
+        val nombre=intent.getStringExtra("nombre")
+        val edad=intent.getIntExtra("edad", 0)
+        println(nombre)
+        println(edad)
+
+        Log.i("intents","Nombre: $nombre")
+        Log.i("intents","Nombre: $edad")
+
+
         btn_actividad_uno.setOnClickListener { this.irActividadMain()
             irActividadMain()
+
         }
     }
 
