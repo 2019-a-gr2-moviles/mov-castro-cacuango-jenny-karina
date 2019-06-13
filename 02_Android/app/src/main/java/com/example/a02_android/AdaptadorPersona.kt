9ab11150c48709cb0f9a766450708cdcb2e9d45d
JennyCastro <jennykarinacastro@gmail.com>
2019-06-13 16:08:37 -0500

    Intent con respuesta

--- 02_Android/app/src/main/java/com/example/a02_android/AdaptadorPersona.kt
+++ 02_Android/app/src/main/java/com/example/a02_android/AdaptadorPersona.kt
@@ -11,35 +11,76 @@ import android.widget.TextView
 import com.example.a02_android.Persona
 import com.example.a02_android.R
 import com.example.a02_android.ReciclerViewActivity
-import kotlinx.android.synthetic.main.layout.*
 
 class AdaptadorPersona(
     private val listaPersonas: List<Persona>,
     private val contexto: ReciclerViewActivity,
     private val recyclerView: RecyclerView
 ) :
-
-
     RecyclerView.Adapter<AdaptadorPersona.MyViewHolder>() {
 
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        var nombreTextView: TextView
+        var cedulaTextView: TextView
+        var accionBoton: Button
+
+        init {
+            nombreTextView = view.findViewById(R.id.txt_nombre) as TextView
+            cedulaTextView = view.findViewById(R.id.txt_cedula) as TextView
+            accionBoton = view.findViewById(R.id.btn_accion) as Button
+
+            val layout = view.findViewById(R.id.linear_layout) as LinearLayout
+
+            layout
+                .setOnClickListener {
+
+                    Log.i("recycler-view", "Layout presionado")
+
+                }
+
+            accionBoton.setOnClickListener {
+                nombreTextView.text = "ME CAMBIAROOOOOOON!!! "
+
+                contexto.cambiarNBombreTextView("WOW!")
+
+                val nuevasPersonas = arrayListOf<Persona>()
+
+                nuevasPersonas.add(Persona("Felipe", "1091231239"))
+                nuevasPersonas.add(Persona("Rafael", "0192929273"))
+                nuevasPersonas.add(Persona("Nydia", "18293041822"))
+
+                contexto.iniciarRecylerView(nuevasPersonas, contexto, recyclerView)
+
+            }
+        }
+
+    }
+
+    // Devuelve el # de items de la lista
     override fun getItemCount(): Int {
-        //regresa cuantos item se tiene en la lista
         return listaPersonas.size
     }
 
-    override fun onBindViewHolder(myViewHolder: AdaptadorPersona.MyViewHolder, position: Int) {
-        //sacar a la persona por la posición
+    override fun onBindViewHolder(
+        myViewHolder: AdaptadorPersona.MyViewHolder,
+        position: Int
+    ) {
+
         val persona = listaPersonas[position]
-        myViewHolder.nombreTextureView.text=persona.nombre
-        myViewHolder.cedulaTextureView.text=persona.cedula
+
+        myViewHolder.nombreTextView.text = persona.nombre
+        myViewHolder.cedulaTextView.text = persona.cedula
 
     }
 
+    // Esta funcion define el template que vamos a utilizar
+    // El template esta en la carpeta de res/layout
     override fun onCreateViewHolder(
         p0: ViewGroup,
-        p1: Int):
+        p1: Int
+    ):
             AdaptadorPersona.MyViewHolder {
-
         val itemView = LayoutInflater
             .from(p0.context)
             .inflate(
@@ -49,32 +90,7 @@ class AdaptadorPersona(
             )
 
         return MyViewHolder(itemView)
-
     }
 
-    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-        //constructor primario
-        var nombreTextureView:TextView
-        var cedulaTextureView:TextView
-        var accionBoton:Button
-
-        init {
-            nombreTextureView=view.findViewById(R.id.txt_nombre) as TextView
-            cedulaTextureView=view.findViewById(R.id.txt_nombre) as TextView
-            accionBoton=view.findViewById(R.id.btn_accion) as Button
-
-            val layout = view.findViewById(R.id.linear_layout) as LinearLayout
-
-            layout
-                .setOnClickListener {
-
-                    Log.i("recycler-view","Layout presionado")
-
-                }
-
-            accionBoton.setOnClickListener { nombreTextureView.text="ME CAMBIAROON !!"}
-        }
-
-    }
 }
--- 02_Android/app/src/main/java/com/example/a02_android/ReciclerViewActivity.kt
+++ 02_Android/app/src/main/java/com/example/a02_android/ReciclerViewActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.example.myapplication.AdaptadorPersona
 import kotlinx.android.synthetic.main.recicler_view.*
 
@@ -14,20 +15,43 @@ class ReciclerViewActivity : AppCompatActivity() {
         setContentView(R.layout.recicler_view)
 
         val lista = arrayListOf<Persona>()
-        val recicler_view = rv_personas
-        val activity= this
+        //val recicler_view = rv_personas
+        //val activity= this
 
         lista.add(Persona("Jenny", "1725052292"))
         lista.add(Persona("Ramiro", "1712363652"))
         lista.add(Persona("Veronica", "1712090164"))
 
-        val adaptadorPersona = AdaptadorPersona(lista, activity, recicler_view)
+        iniciarRecylerView(lista, this, rv_personas)
 
-        recicler_view.adapter=adaptadorPersona
-        rv_personas.itemAnimator=DefaultItemAnimator()
-        rv_personas.layoutManager=LinearLayoutManager(this)
+        //val adaptadorPersona = AdaptadorPersona(lista, activity, recicler_view)
+
+        //recicler_view.adapter=adaptadorPersona
+        //rv_personas.itemAnimator=DefaultItemAnimator()
+        //rv_personas.layoutManager=LinearLayoutManager(this)
 
         //Coge la posición y las va poniendo en los campos de nombre y cedula
+        //adaptadorPersona.notifyDataSetChanged()
+    }
+
+    fun cambiarNBombreTextView(texto: String){
+        txv_titulo_rv.text = texto
+    }
+
+    fun iniciarRecylerView(
+        lista: List<Persona>,
+        actividad: ReciclerViewActivity,
+        recycler_view: RecyclerView
+    ) {
+        val adaptadorPersona = AdaptadorPersona(
+            lista,
+            actividad,
+            recycler_view
+        )
+        recycler_view.adapter = adaptadorPersona
+        recycler_view.itemAnimator = DefaultItemAnimator()
+        recycler_view.layoutManager = LinearLayoutManager(actividad)
+
         adaptadorPersona.notifyDataSetChanged()
     }
 }
